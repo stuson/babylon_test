49a6e314de6359047e2713c5364c95a2a3823cd8
Sam <sam.tuson@hotmail.co.uk>
2018-09-29 21:30:36 +0100

    Set up render

---
+++ script/game.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById('render-canvas')
+const engine = new BABYLON.Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true
+})
+
+function createScene() {
+    let scene = new BABYLON.Scene(engine)
+    
+    let camera = new BABYLON.ArcRotateCamera('camera1', new BABYLON.Vector3(0, 6, -20), scene)
+    camera.setTarget(BABYLON.Vector3.Zero())
+    camera.attachControl(canvas, true)
+
+    let light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene)
+
+    let sphere = BABYLON.MeshBuilder.CreateBox('player', {
+        size: 4,
+        sideOrientation: BABYLON.Mesh.FRONTSIDE
+    }, scene)
+    sphere.position.y = 2
+
+    let ground = BABYLON.MeshBuilder.CreateGround('ground1', {
+        width: 60,
+        height: 6,
+        subdivisions: 2,
+        sideOrientation: BABYLON.Mesh.FRONTSIDE
+    }, scene)
+
+    return scene
+}
+
+window.addEventListener('resize', function() {
+    engine.resize()
+})
+
+let scene = createScene()
+engine.runRenderLoop(function() {
+    scene.render()
+})
